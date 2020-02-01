368bd6ddd742927418102711a9ca9ab6df8c0ee8
MGraf <info@moritz-graf.de>
2020-02-01 13:23:44 +0100

    Demo Page

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -4,12 +4,16 @@ module.exports = {
   rootDir: resolve(__dirname, '..'),
   buildDir: resolve(__dirname, '.nuxt'),
   srcDir: __dirname,
+  mode: 'universal',
   render: {
     resourceHints: false
   },
   modules: [
     '@@'
   ],
+  generate: {
+    dir: '../dist'
+  },
   build: {
     html: {
       minify: {
